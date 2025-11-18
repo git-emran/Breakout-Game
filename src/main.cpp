9c22ec190298a965235dff377ffb313242f3685d
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-11-18 14:49:03 +0600

    scaffolding the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include "raylib.h"
 
+struct Ball {
+  Vector2 pos;
+  Vector2 acceleration{1.0f, 1.0f};
+  float velocity;
+  float radius = 5.0f;
+};
+
+struct Brick {};
+struct Player {};
+
+void GameStartup() {}
+
+void GameUpdate() {}
+
+void GameRender() {}
+
+void GameShutDown() {}
+
 int main() {
 
-  const int screenWidth = 800;
-  const int screenHeight = 450;
-
-  InitWindow(screenWidth, screenHeight, "First Raylib project");
-  SetTargetFPS(60);
+  const int screenWidth = 700;
+  const int screenHeight = 600;
 
   while (!WindowShouldClose()) {
     BeginDrawing();
